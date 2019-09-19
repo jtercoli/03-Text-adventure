5062a69ec16aeb4e6015c991d2ccc66a60e498a7
Jadan Ercoli <jtercoli@iu.edu>
2019-09-18 23:31:51 -0400

    Updates game.json, main.py, and README.md

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # 03-Text-adventure
- Creating a game engine capable of generating an Interactive Fiction game based on an external description file.
+ Hey all, 
+ In this file we have a game engine we created in-class and a small Interactive Fiction game.
+ A lot of this game was built using a backbone that our teacher, Mr. Francis, made for us so that we may begin exploring our creative interests. The only directions are North, East, South, and West, but there are plenty of rooms to explore! Typing 'Quit' will get you out of the program immediately if you run into a death sequence or you just really hate the game!
--- main.py
+++ main.py
@@ -9,55 +9,132 @@ assert sys.version_info >= version, "This script requires at least Python {0}.{1
 logging.basicConfig(format='[%(filename)s:%(lineno)d] %(message)s', level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
+# Game loop functions
+def render(room,moves,points):
+    ''' Displays the current room, moves, and points '''
 
-def render(world,current_location):
-    '''print out description of current location'''
-    room = world[current_location]
-    print (room["name"])
-    print (room["desc"])
-    return True
+    print('\n\nIf you are dead or stuck, I would suggest typing "quit" :^)')
+    print('\n\nMoves: {moves}, Points: {points}'.format(moves=moves, points=points))
+    print('\n\nYou are {name}'.format(name=room['name']))
+    
+    print(room['desc'])
+    if len(room['inventory']):
+        print('You see the following items:')
+        for i in room['inventory']:
+            print('\t{i}'.format(i=i))
+
+def getInput(verbs):
+    ''' Asks the user for input and normalizes the inputted value. Returns a list of commands '''
+
+    response = input('\nWhat would you like to do? (You may go North, East, South, or West) ').strip().upper().split()
+    if (len(response)):
+        #assume the first word is the verb
+        response[0] = normalizeVerb(response[0],verbs)
+    return response
 
 
-def check_input():
-    '''request input from the player, validate the input'''
-    user_input = input("\nWhat would you like to do? ").strip().upper()
-    # do we want to validate?
-    return user_input
-
-def update(selection,game,current):
-    '''Check if we need to move to a new location, etc..'''
-    for e in game ['rooms'][current]['exits']:
-        if e['verb'] == selection:
-            if e['target'] == 'NoExit':
-                print('You cannot go that way')
-            else:
-                current = e['target']
-            
+def update(response,room,current,inventory):
+    ''' Process the input and update the state of the world '''
+    s = list(response)[0]  #We assume the verb is the first thing typed
+    if s == "":
+        print("\nSorry, I don't understand.")
+        return current
+    elif s == 'EXITS':
+        printExits(room)
+        return current
+    else:
+        for e in room['exits']:
+            if s == e['verb'] and e['target'] != 'NoExit':
+                return e['target']
+    print("\nYou can't go that way!")
     return current
+
+# Helper functions
+
+def printExits(room):
+    e = ", ".join(str(x['verb']) for x in room['exits'])
+    print('\nYou can go the following directions: {directions}'.format(directions = e))
+
+def normalizeVerb(selection,verbs):
+    for v in verbs:
+        if selection == v['v']:
+            return v['map']
+    return ""
+
+def end_game(winning,points,moves):
+    if winning:
+        print('\n\nYou have won! Congratulations')
+        print('\nYou scored {points} points in {moves} moves! Nicely done!'.format(moves=moves, points=points))
+    else:
+        print('\n\nThanks for playing!')
+        print('\nYou scored {points} points in {moves} moves. See you next time!'.format(moves=moves, points=points))
+
+
+
 
 
 def main():
+
+    # Game name, game file, starting location, winning location(s), losing location(s)
+    games = [
+        (   "Dracula's Lair - Jadan Ercoli",          'game.json',    'START',    ['END'],    [])
+        ,(  'Zork I',           'zork.json',    'WHOUS',    ['NIRVA'],  [])
+        ,(  'A Nightmare',      'dream.json',    'START',   ['AWAKE'],  ['END'])
+    ]
+
+    # Ask the player to choose a game
     game = {}
-    with open('zork.json') as json_file:
+    while not game:
+        print('\n\nWhich game would you like to play?\n')
+        for i,g in enumerate(games):
+            print("{i}. {name}".format(i=i+1, name=g[0]))
+        try:
+            choice = int(input("\nPlease select an option: "))
+            game = games[choice-1]
+        except:
+            continue
+            
+    name,gameFile,current,win,lose = game
+
+
+
+
+
+    with open(gameFile) as json_file:
         game = json.load(json_file)
-    # Your game goes here!
 
-    current = 'WHOUS'
+    moves = 0
+    points = 0
+    inventory = []
 
-    quit = False
-    while not quit:
-        #render the world
-        render(game['rooms'],current)
-        #check for player input
-        selection = check_input()
-        #update the state of the world
-        current = update(selection,game,current)
+    print("\n\n\n\nWelcome to {name}!\n\n".format(name=name))
+    while True:
+      
+        render(game['rooms'][current],moves,points)
+        
         
 
-    return True
+        response = getInput(game['verbs'])
+
+        if response[0] == 'QUIT':
+            end_game(False,points,moves)
+            break
+
+        current = update(response,game['rooms'][current],current,inventory)
+
+        moves += 1
+    
+        if current in win:
+            end_game(True,points,moves)
+            break
+        if current in lose:
+            end_game(False,points,moves)
+            break
 
 
 
-#if we are running this from the command line, run main
+
+
+
 if __name__ == '__main__':
 	main()
